5c8d080d3427f52f5407c628c22a2a3b5985514e
Felipe <felipezb@live.com>
2019-04-15 17:27:18 +0300

    Removed public URLs

--- background.js
+++ background.js
@@ -1,8 +1,8 @@
 chrome.browserAction.onClicked.addListener(function (tab) {
 
-    var googleScriptUrl = "https://script.google.com/macros/s/AKfycbxmlPDQHBSTcrBHKlin3-8-K-m3BYdgKjWWZBNXKkKK2jEIEuZF/exec";
-    var sheetUrl = "https://docs.google.com/spreadsheets/d/16DvU6M6-FCtTA7ZL4uVIrSVXE884IjvtRFvpAF_6rYk/edit?usp=sharing";
-    var sheetName = "bookmarks";
+    var googleScriptUrl = "Your Google Script URL";
+    var sheetUrl = "Your Google Sheet URL";
+    var sheetName = "Your sheet(tab) name";
     var url = tab.url;
     var title = tab.title;
     var url = googleScriptUrl + "?action=insert&title=" + title + "&url=" + url + "&sheetUrl=" + sheetUrl + "&sheetName=" + sheetName;
