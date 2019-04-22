ec9dce834ff4e77c8275df58e105fd0b9e7e459d
felipe <felipezb@live.com>
2019-04-22 15:58:27 +0300

    Updated badge status

--- background.js
+++ background.js
@@ -1,8 +1,8 @@
 var gScriptUrl =
   "https://script.google.com/macros/s/AKfycbxmlPDQHBSTcrBHKlin3-8-K-m3BYdgKjWWZBNXKkKK2jEIEuZF/exec";
 
-  chrome.runtime.onInstalled.addListener(function () {
-  chrome.tabs.create({url: "options.html"}); 
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.tabs.create({ url: "options.html" });
 });
 
 chrome.browserAction.onClicked.addListener(function(tab) {
@@ -25,7 +25,27 @@ chrome.browserAction.onClicked.addListener(function(tab) {
 
 function insert() {
   chrome.browserAction.setBadgeText({ text: "sync" });
-  fetch(insertUrl).then(function() {
-    chrome.browserAction.setBadgeText({ text: "" });
-  });
+  fetch(insertUrl)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(result) {
+      if (result.result == "success") {
+        chrome.browserAction.setBadgeText({ text: "OK" });
+        chrome.browserAction.setBadgeBackgroundColor({ color: "#00A572" });
+      } else {
+        chrome.browserAction.setBadgeText({ text: "ERROR" });
+        chrome.browserAction.setBadgeBackgroundColor({ color: "#b80f0a" });
+      }
+      timer = setTimeout(function() {
+        chrome.browserAction.setBadgeText({ text: "" });
+      }, 2000);
+    })
+    .catch(function(error) {
+      chrome.browserAction.setBadgeText({ text: "ERROR" });
+      chrome.browserAction.setBadgeBackgroundColor({ color: "#b80f0a" });
+      timer = setTimeout(function() {
+        chrome.browserAction.setBadgeText({ text: "" });
+      }, 2000);
+    });
 }
